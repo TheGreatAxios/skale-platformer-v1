3e8f16742c57bbb83ded625f78acbe9274eb4c38
TheGreatAxios <sawyer@dirtroad.dev>
2023-05-16 21:54:17 -0700

    Fix endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const isProduction = true;
 /// https://mainnet.skalenodes.com
 ////http://54.39.185.56:10131
-export const RPC_URL = isProduction ? "https://170.64.154.250/v1/green-giddy-denebola" : "https://staging-v3.skalenodes.com/v1/staging-utter-unripe-menkar";
+export const RPC_URL = isProduction ? "http://170.64.154.250/v1/green-giddy-denebola" : "https://staging-v3.skalenodes.com/v1/staging-utter-unripe-menkar";
 
 export const SFUEL_KEY = isProduction ? "nebula-mainnet" : "calypso-testnet";
