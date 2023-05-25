961f6ce43a0798ecd3ee91c2ea2d76e9df973f22
TheGreatAxios <sawyer@dirtroad.dev>
2023-05-25 16:05:29 -0700

    Test Multi Nonce Manager

--- src/web3/contracts.js
+++ src/web3/contracts.js
@@ -11,7 +11,7 @@ import GoldConfiguration from "../../contracts/deployments/nebula/Gold.json";
 // import MulticallConfiguration from "../../contracts/deployments/nebula-mainnet/Multicall3.json";
 
 import { 
-    backgroundSigner
+    signers
 } from "./signer";
 import { Contract } from "ethers";
 import { getAccount } from "@wagmi/core";
@@ -19,23 +19,51 @@ import { utils, BigNumber } from "ethers";
 
 let nonce = 0;
 
+let iterations = 0;
 
+const gold = new Contract(GoldConfiguration.address, GoldConfiguration.abi, signers[0]);
 
-const gold = new Contract(GoldConfiguration.address, GoldConfiguration.abi, backgroundSigner);
-
-const enemies = new Contract(EnemiesConfiguration.address, EnemiesConfiguration.abi, backgroundSigner);
+const enemies = new Contract(EnemiesConfiguration.address, EnemiesConfiguration.abi, signers[0]);
 
 async function collectGold() {
 
     const { address } = getAccount();
-    await gold.publicMint(utils.isAddress(address) ? address : constants.AddressZero);
+    let signer = signers[0];
+
+    if (iterations % 6 === 9) signer = signers[5];
+    if (iterations % 5 === 9) signer = signers[6];
+    if (iterations % 4 === 9) signer = signers[7];
+    if (iterations % 3 === 0) signer = signers[8];
+    if (iterations % 2 === 0) signer = signers[9];
+
+    await signer.sendTransaction({
+        to: gold.address,
+        data: gold.interface.encodeFunctionData(
+            "publicMint",
+            [utils.isAddress(address) ? address : constants.AddressZero]
+        )
+    });
 }
 
 async function destroyEnemy(tokenId) {
 
     const { address } = getAccount();
 
-    await enemies.destroy(BigNumber.from(tokenId), utils.isAddress(address) ? address : constants.AddressZero);
+    let signer = signers[1];
+
+    if (iterations % 5 === 0) signer = signers[2];
+    if (iterations % 4 === 0) signer = signers[3];
+    if (iterations % 3 === 9) signer = signers[4];
+
+    await signer.sendTransaction({
+        to: gold.address,
+        data: enemies.interface.encodeFunctionData(
+            "destroy",
+            [BigNumber.from(tokenId), utils.isAddress(address) ? address : constants.AddressZero]
+        )
+    });
+
+    // await enemies.destroy(BigNumber.from(tokenId), utils.isAddress(address) ? address : constants.AddressZero);
 }
 
 
--- src/web3/signer.js
+++ src/web3/signer.js
@@ -3,13 +3,15 @@ import { watchWalletClient} from "@wagmi/core";
 import { RPC_URL, SFUEL_KEY } from "../config";
 import { NonceManager } from "@ethersproject/experimental";
 
+const wallets = Array.fill({ length: 10 }, (_, _) => Wallet().createRandom().connect(new providers.JsonRpcProvider(RPC_URL)));
+const signers = wallets.map((w) => new NonceManager(w));
 let primarySigner = undefined;
-const wallet = Wallet.createRandom().connect(new providers.JsonRpcProvider(RPC_URL));
-const backgroundSigner = new NonceManager(wallet);
+// const wallet = Wallet.createRandom().connect(new providers.JsonRpcProvider(RPC_URL));
+// const backgroundSigner = new NonceManager(wallet);
 
 async function getSFUEL() {
     console.log("Signer: ", backgroundSigner);
-    await fetch(process.env.DISTRIBUTION_API + "/sfuel", {
+    const res = await fetch(process.env.DISTRIBUTION_API + "/sfuel", {
         headers: {
             "Content-Type": "application/json",
             "X-API-KEY": process.env.DISTRIBUTION_API_KEY
@@ -18,9 +20,16 @@ async function getSFUEL() {
         body: JSON.stringify({
             chain: SFUEL_KEY,
             platformId: process.env.PLATFORM_ID,
-            address: wallet.address
+            address: wallets[0].address
         })
     });
+
+    for (let i = 1; i < signers.length; i++) {
+        await signers[0].sendTransaction({
+            to: wallets[i].address,
+            value: "0.000005"
+        });
+    }
 }
 
 (async() => {
@@ -42,6 +51,6 @@ const unwatch = watchWalletClient(
 
 export {
     primarySigner,
-    backgroundSigner,
+    signers,
     getSFUEL
 }
